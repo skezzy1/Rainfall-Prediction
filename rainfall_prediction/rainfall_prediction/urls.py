1383d5d295e1815048b799cce72cbea85932e20e
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-10-15 21:30:08 +0300

    Updated list of urls

--- rainfall_prediction/rainfall_prediction/urls.py
+++ rainfall_prediction/rainfall_prediction/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+#from weather import views as weather 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('', weather.home, name='home'),
+    path('', include('weather.urls')),
 ]
