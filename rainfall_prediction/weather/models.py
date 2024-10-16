8e2f7dc226091211fdfdbc400768a9e813de748b
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-10-16 23:07:28 +0300

    New main file

--- rainfall_prediction/weather/models.py
+++ rainfall_prediction/weather/models.py
@@ -1,14 +1,3 @@
 from django.db import models
 
-class CityWeather(models.Model):
-    city = models.CharField(max_length=100)
-    date = models.DateField()
-    temperature = models.FloatField()
-    humidity = models.FloatField()
-    pressure = models.FloatField()
-    wind_speed = models.FloatField()
-    clouds = models.FloatField()
-    rain = models.FloatField()
-
-    def __str__(self):
-        return self.city
+# Create your models here.
