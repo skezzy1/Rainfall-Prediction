d0fc32829e81cf816995f3021fe54dff70f9c848
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-10-16 23:09:13 +0300

    New path on the site

--- rainfall_prediction/weather/urls.py
+++ rainfall_prediction/weather/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Головна сторінка
+   # path('forecast/', views.forecast, name='forecast'),  # Сторінка з прогнозами
 ]
