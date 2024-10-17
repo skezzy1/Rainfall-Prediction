ebcf4b1a70921b23b788a4305e9dbab62afb58a9
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-10-17 10:40:27 +0300

    Updated path

--- rainfall_prediction/rainfall_prediction/urls.py
+++ rainfall_prediction/rainfall_prediction/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from weather import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Маршрут до адмін-панелі
-    path('', include('weather.urls')),  # Маршрут до додатку
+    path('', include('weather.urls')),  # Маршрут до додатку weather
 ]
 
